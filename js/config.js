8eb64cf6c4e3d2f25f452c3013795e74f9927d67
danielantunesserranalog-eng <daniel.antunesserranalog@gmail.com>
2026-04-30 17:00:22 -0300

    atualizei

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Arquivo: js/config.js
+window.ENV = {
+    // URL do seu projeto Supabase
+    SUPABASE_URL: 'https://qhbenzrxajbeaatwxtlj.supabase.co',
+    
+    // Sua chave anônima (anon key)
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYmVuenJ4YWpiZWFhdHd4dGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MzQ2OTksImV4cCI6MjA5MjMxMDY5OX0.2ddgnsjmxqmX9xk68m9duUmzAO2n2OAvEpOgHevRwkU'
+};
